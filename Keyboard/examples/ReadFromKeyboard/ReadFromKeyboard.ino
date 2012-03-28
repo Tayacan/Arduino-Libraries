15b92d9d6bf4c9c7afd571ff7e6088efd3c73373
Maya Saietz <mayasaietz@yahoo.dk>
2012-03-28 15:01:48 +0300

    Comments! Yay!

--- Keyboard/examples/ReadFromKeyboard/ReadFromKeyboard.ino
+++ Keyboard/examples/ReadFromKeyboard/ReadFromKeyboard.ino
@@ -1,5 +1,6 @@
 #include <Keyboard.h>
 
+// The numbers are pins. Modify depending on your setup.
 Keyboard k(10,9,8,2,3,4,5);
 
 void setup() 
@@ -9,7 +10,9 @@ void setup()
 
 void loop() 
 {
+  // Get a keycode from the keyboard
   int inp = k.Read();
+  // 0 means no input
   if(inp != 0)
   {
     Serial.println(inp);
